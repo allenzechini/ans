e41c5939697145286b1ab1facafa5b1bff73cbbd
Allen Zechini <allen@shotover.com>
2021-05-18 20:28:05 +0000

    Added argparser

--- get-pass.py
+++ get-pass.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+import argparse
 import subprocess
 
-def get_vault_key(search='test-pw-1'):
-  command = 'bw get password %s' % search
+def get_key(search):
+  command = 'bw get password {}'.format(search)
   key = subprocess.run(command.split(), check=True, stdout=subprocess.PIPE)
   return key.stdout.decode('utf-8')
 
 def main():
-  print(get_vault_key())
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--item', help='Retrieve password from this item')
+  args = parser.parse_args()
+
+  # bw_login()
+  print(get_key(args.item))
 
 if __name__ == '__main__':
   main()
