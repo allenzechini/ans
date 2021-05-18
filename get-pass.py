a6cb7514c5013bf83391aded79e9eb61963f9960
Allen Zechini <allen@shotover.com>
2021-05-18 22:11:01 +0000

    Added logic to check vault status

--- get-pass.py
+++ get-pass.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import argparse
 import subprocess
 
-def get_key(search):
-  command = 'bw get password {}'.format(search)
-  key = subprocess.run(command.split(), check=True, stdout=subprocess.PIPE)
-  return key.stdout.decode('utf-8')
+def is_logged_in():
+  if 'BW_SESSION' not in os.environ:
+    cmd = 'bw status'
+    out = subprocess.run(cmd.split(), check=True, stdout=subprocess.PIPE)
+    if 'unauthenticated' in out.stdout.decode('utf-8'):
+      print('Please login to your vault using \'bw login\', '
+            'set the environment variable BW_SESSION, '
+            'then re-run your command')
+      sys.exit(1)
+    elif 'locked' in out.stdout.decode('utf-8'):
+      print('Please unlock your vault using \'bw unlock\', '
+            'set the environment variable BW_SESSION, '
+            'then re-run your command')
+      sys.exit(2)
+  else:
+    return True
+
+def get_p(search):
+  cmd = 'bw get password {}'.format(search)
+  p = subprocess.run(cmd.split(), check=True, stdout=subprocess.PIPE)
+  return p.stdout.decode('utf-8')
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-i', '--item', help='Retrieve password from this item')
   args = parser.parse_args()
 
-  # bw_login()
-  print(get_key(args.item))
+  if is_logged_in():
+    print(get_p(args.item))
 
 if __name__ == '__main__':
   main()
