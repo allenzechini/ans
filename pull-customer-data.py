a9fa4822d540c7ede3c1d3fad467684b67cf0058
Allen Zechini <allen@shotover>
2021-07-28 17:27:30 -0600

    Comment block on tuples for storing ansible dict info

--- pull-customer-data.py
+++ pull-customer-data.py
@@ -1,44 +1,23 @@
-import requests
-import json
 import os
-#from requests_oauthlib import OAuth2Session
+import json
+import requests
 
 # ID vars
-# Client/JIRA info received from provider (Atlassian)
-#clientID = os.environ.get('PYAPP_CLIENTID')
-#clientSecret = os.environ.get('PYAPP_CLIENTSECRET')
 basic_auth  = os.environ.get('JIRA_BASICAUTH')
 workspaceID = os.environ.get('JIRA_WORKSPACEID')
-ObjectIDs   = {
-  "ATOM": "22"
-}
 
-# URI vars
-#redirectURI = 'https://shotoverjira.atlassian.net'
-#authbaseURI = 'https://auth.atlassian.com/authorize'
-#accesstokenURI = 'https://auth.atlassian.com/oauth/token'
-#insightAPIURI = f'{redirectURI}/jsm/insight/workspace/{workspaceID}/v1/object/{ObjectIDs["ATOM"]}'
-#scopes = 'read:servicedesk-request read:servicemanagement-insight-objects'
+#ObjectIDs   = {
+#  "ATOM": "22"
+#}
 
-# Create initial auth request
-#oauth = OAuth2Session(clientID, redirect_uri=redirectURI, scope=scopes)
-#authorization_url, state = oauth.authorization_url(authbaseURI)
-#print(f'Go to {authorization_url} to authorize access')
-#authorization_response = input('Enter the full callback URL: ')
-#
-## Fetch access token
-#token = oauth.fetch_token(
-#  accesstokenURI,
-#  authorization_response=authorization_response,
-#  client_secret=clientSecret
-#)
-#
-## Request data from resource
-#response = oauth.get(insightAPIURI)
-#response.json()
+# Instead of using dictionaries, thinking of using tuples, like this...
+# ATOM = ("ATOM", 22, 308, 312)
+# Where:
+#   "ATOM" = name of product
+#   22     = product_id
+#   308    = name_id
+#   312    = org_id
 
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
 url = "https://api.atlassian.com/jsm/insight/workspace/{workspaceId}/v{version}/object/navlist/iql"
 headers = {
   "Accept": "application/json",
