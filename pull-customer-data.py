e8267e7981bfb31161799d3fe3ef78a2d666fdfd
Allen Zechini <allen@shotover>
2021-07-28 16:46:40 -0600

    Create function get_products in pull-customer-data.py

--- pull-customer-data.py
+++ pull-customer-data.py
@@ -42,32 +42,38 @@ ObjectIDs   = {
 url = "https://api.atlassian.com/jsm/insight/workspace/{workspaceId}/v{version}/object/navlist/iql"
 headers = {
   "Accept": "application/json",
+  "Authorization": "Basic " + basic_auth,
   "Content-Type": "application/json"
 }
 
-payload = json.dumps({
-  "objectTypeId": "23",
-  "attributesToDisplay": {
-    "attributesToDisplayIds": [
-      135,
-      144
-    ]
-  },
-  "page": 1,
-  "asc": 1,
-  "resultsPerPage": 25,
-  "includeAttributes": false,
-  "objectSchemaId": "6",
-  "iql": "objectType = Office AND Name LIKE SYD"
-})
+def get_products(product="ATOM", product_id="22", name_id="308", org_id="312"):
+  payload = json.dumps({
+    "iql": "objectType = " + product,
+    "objectTypeId": product_id,
+    "page": 1,
+    "resultsPerPage": 600,
+    "includeAttributes": true,
+    "attributesToDisplay": {
+      "attributesToDisplayIds": [
+        name_id,
+        ord_id
+      ]
+    },
+    "objectSchemaId": "4"
+  })
 
-response = requests.request(
-  "POST",
-  url,
-  data=payload,
-  headers=headers
-)
+  response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers
+  )
 
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+  print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+#def main():
+#  call API
+#  print to file
+#
 #if __name__ == '__main__':
 #  main()
